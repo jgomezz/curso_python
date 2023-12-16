48a649b70f4cc6fd79a6c69fde80b5f7380a5995
jgomezz <jgomezz@gmail.com>
2023-12-16 17:00:41 -0500

    first commit

---
+++ codigos-semana-03/ejercicios.py
@@ -0,0 +1,29 @@
+
+class Auto:
+    
+    def __init__(self, color1, marca1, anho1) :
+        self.color = color1
+        self.marca = marca1
+        self.anho = anho1
+
+
+auto_toyota =  Auto("Blanco","Toyota",2020)
+
+print(auto_toyota.color)
+print(auto_toyota.marca)
+print(auto_toyota.anho)
+
+
+auto_nissan =  Auto("Negro","Nissan",2021)
+
+print(auto_nissan.color)
+print(auto_nissan.marca)
+print(auto_nissan.anho)
+
+
+exit(-1)
+
+"""
+Agregar el color y la marca en la clae Auto, y pasarselo por el constructor.
+
+"""
