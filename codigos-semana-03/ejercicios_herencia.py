e71b57b912538960fdd882dde8ba56513a830177
jgomezz <jgomezz@gmail.com>
2023-12-23 14:24:59 -0500

    Update

--- codigos-semana-03/ejercicios_herencia.py
+++ codigos-semana-03/ejercicios_herencia.py
@@ -18,7 +18,16 @@ class Auto:
         self.garantia += extra_garantia         
         if self.ciudad == "Lima" and self.garantia > 2:  
             self.garantia = 2 
-    
+
+# Crear clase hija
+            
+class SUV (Auto) :
+    pass
+
 auto_hyundai = Auto("Blanco","Hyundai","2022","Lima" )
 
 auto_hyundai.imprimir_info()
+
+suv_hyundai = SUV("Negro","Hyundai","2023","Arequipa" )
+
+suv_hyundai.imprimir_info()
