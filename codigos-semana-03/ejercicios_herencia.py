e92f0ef55d76f3e0cbece085cc69c0faeec4f107
jgomezz <jgomezz@gmail.com>
2023-12-23 14:20:35 -0500

    Update

--- codigos-semana-03/ejercicios_herencia.py
+++ codigos-semana-03/ejercicios_herencia.py
@@ -1,12 +1,11 @@
 class Auto:
     
-    ciudad = "Lima"
-
-    def __init__(self, color1, marca1, anho1) :
-        self.color = color1
-        self.marca = marca1
-        self.anho = anho1
+    def __init__(self, color, marca, anho, ciudad) :
+        self.marca = marca
+        self.color = color
+        self.anho = anho
         self.garantia = 1  # Garantia del auto
+        self.ciudad = ciudad
 
     def imprimir_info(self):
         print("La marca es " , self.marca , " y el anho es " , self.anho)
@@ -15,17 +14,11 @@ class Auto:
         print("La marca es " , self.marca , ", el anho es " , 
               self.anho , " y  el color ", self.color)
 
-    def obtener_info(self):
-        return self.marca +  "|" +  self.color
-
     def incrementar_garantia(self, extra_garantia):  
         self.garantia += extra_garantia         
         if self.ciudad == "Lima" and self.garantia > 2:  
             self.garantia = 2 
     
-    @classmethod
-    def modificar_ciudad(cls, nueva_ciudad):
-        cls.ciudad =  nueva_ciudad
+auto_hyundai = Auto("Blanco","Hyundai","2022","Lima" )
 
-
-
+auto_hyundai.imprimir_info()
