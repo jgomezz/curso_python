76f5c80c787f5b397db8cf59de12db993041af83
jgomezz <jgomezz@gmail.com>
2023-12-23 16:28:41 -0500

    Update

--- codigos-semana-04/data.sql
+++ codigos-semana-04/data.sql
@@ -1,9 +1,9 @@
 CREATE TABLE employees (
-	emp_no int(11) NOT NULL AUTO_INCREMENT, 
+    emp_no int(11) NOT NULL AUTO_INCREMENT, 
     first_name varchar(14) NOT NULL,
-	last_name varchar(16) NOT NULL,
-	gender enum('H','M') NOT NULL,
-	birth_date date ,
-	hire_date date ,
-	PRIMARY KEY (emp_no)
+    last_name varchar(16) NOT NULL,
+    gender enum('H','M') NOT NULL,
+    birth_date date ,
+    hire_date date ,
+    PRIMARY KEY (emp_no)
  )
